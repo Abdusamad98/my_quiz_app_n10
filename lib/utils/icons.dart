6c8004fe6cca7c8dee4afce34a8ff6f3dcc1c385
Abdushukur <asrolov01@mail.ru>
2024-06-14 15:26:42 +0500

    Change color

--- lib/utils/icons.dart
+++ lib/utils/icons.dart
@@ -1,7 +1,7 @@
 class AppImages {
   //png
   static const String bicycle = "assets/images/bicycle.png";
-  static const String logo = "assets/images/logo.png";
+  static const String logo = "assets/images/ic_launcher.png";
   static const String english = "assets/images/english.png";
   static const String flutter = "assets/images/flutter.png";
 
